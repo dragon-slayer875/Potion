56c30305f2b733827b55790888a84c3987c0c52e
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-02-03 23:20:39 +0530

    feat: add supabase upload function
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

---
+++ src/lib/supabase.ts
@@ -0,0 +1,19 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL || "";
+const supabaseKey = process.env.SUPABASE_PROJECT_KEY || "";
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+export async function uploadImage(image: Blob) {
+  const { data, error } = await supabaseClient.storage
+    .from("potion-storage")
+    .upload(`${new Date().getTime()}.png`, image);
+
+  if (error) {
+    console.error(error);
+    alert("Failed to upload image");
+  }
+
+  return data;
+}
