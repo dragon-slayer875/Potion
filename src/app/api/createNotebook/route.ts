017b77fb4a9f3e1a45be74b7ab9a2c88a2538a89
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-02-04 22:07:11 +0530

    chore: wrap create notebook endpoint in try catch block
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/app/api/createNotebook/route.ts
+++ src/app/api/createNotebook/route.ts
@@ -7,36 +7,44 @@ import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { userId } = await auth();
-  if (!userId) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
-  const body = await req.json();
-  const { name } = body;
-  const image_description = await generateImagePrompt(name);
-  if (!image_description) {
-    return new NextResponse("Failed to generate image description", {
-      status: 500,
-    });
-  }
-  const imageUrl = await generateImage(image_description);
-  if (!imageUrl) {
-    return new NextResponse("Failed to generate image", {
-      status: 500,
-    });
-  }
-  const notebook = await db
-    .insert($notebooks)
-    .values({
-      name,
-      userId,
-      imageUrl,
-    })
-    .returning({
-      id: $notebooks.id,
-    });
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 
-  return NextResponse.json({
-    notebookId: notebook[0].id,
-  });
+    const body = await req.json();
+    const { name } = body;
+    const image_description = await generateImagePrompt(name);
+    if (!image_description) {
+      return new NextResponse("Failed to generate image description", {
+        status: 500,
+      });
+    }
+
+    const imageUrl = await generateImage(image_description);
+    if (!imageUrl) {
+      return new NextResponse("Failed to generate image", {
+        status: 500,
+      });
+    }
+
+    const notebook = await db
+      .insert($notebooks)
+      .values({
+        name,
+        userId,
+        imageUrl,
+      })
+      .returning({
+        id: $notebooks.id,
+      });
+
+    return NextResponse.json({
+      notebookId: notebook[0].id,
+    });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Failed to create notebook", { status: 500 });
+  }
 }
