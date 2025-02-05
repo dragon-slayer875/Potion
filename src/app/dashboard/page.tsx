2d73a6b3fb2ff6fab2feda3666ee02f058a5ebef
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-02-05 16:58:02 +0530

    feat: display existing notebooks in dashboard
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,24 @@
 import CreateNoteDialog from "@/components/CreateNoteDialog";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { db } from "@/lib/db";
+import { $notebooks } from "@/lib/db/schema";
 import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-const DashboardPage = (props: Props) => {
+const DashboardPage = async (props: Props) => {
+  const { userId } = await auth();
+  const notebooks = await db
+    .select()
+    .from($notebooks)
+    .where(eq($notebooks.userId, userId!));
   return (
     <>
       <div className="grainy min-h-screen">
@@ -31,12 +41,39 @@ const DashboardPage = (props: Props) => {
           <div className="h-8"></div>
           <Separator />
           <div className="h-8"></div>
-          {/* list of all notes */}
-          <div className="text-center">
-            <h2 className="text-xl text-gray-500">You have no notes yet.</h2>
-          </div>
+          {notebooks.length === 0 && (
+            <div className="text-center">
+              <h2 className="text-xl text-gray-500">You have no notes yet.</h2>
+            </div>
+          )}
           <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-1 gap-3">
             <CreateNoteDialog />
+            {notebooks.map((notebook) => {
+              return (
+                <Link href={`/notebook/${notebook.id}`} key={notebook.id}>
+                  <div className="border border-stone-200 overflow-hidden rounded-lg flex flex-col hover:shadow-xl transition hover:-translate-y-1">
+                    <Image
+                      src={notebook.imageUrl || ""}
+                      alt={notebook.name}
+                      width={400}
+                      height={200}
+                      style={{
+                        width: "auto",
+                      }}
+                    />
+                    <div className="p-4">
+                      <h3 className="text-xl font-semibold text-gray-900">
+                        {notebook.name}
+                      </h3>
+                      <div className="h-1"></div>
+                      <p className="text-sm text-gray-500">
+                        {new Date(notebook.createdAt).toLocaleDateString()}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </div>
