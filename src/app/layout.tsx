6af2c43d978682cf32eef75788ab2a4871ca1f86
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-02-05 19:34:34 +0530

    chore: remove clerk dev mode warning
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html lang="en">
         <Provider>
           <body
