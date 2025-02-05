d706cd622dd9fe6ee482427078d14cc777fbf6f3
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-02-05 15:21:23 +0530

    feat: add ai completion function
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,8 +1,9 @@
-import Groq from "groq-sdk";
 import { HfInference } from "@huggingface/inference";
 import { uploadImage } from "./supabase";
+import { createGroq } from "@ai-sdk/groq";
+import { generateText, streamText } from "ai";
 
-const groq = new Groq({
+const groq = createGroq({
   apiKey: process.env.GROQ_API_KEY,
 });
 
@@ -10,8 +11,8 @@ const hfClient = new HfInference(process.env.HGF_TOKEN);
 
 export async function generateImagePrompt(name: string) {
   try {
-    const response = await groq.chat.completions.create({
-      model: "llama-3.3-70b-versatile",
+    const response = await generateText({
+      model: groq("llama-3.3-70b-versatile"),
       messages: [
         {
           role: "system",
@@ -24,7 +25,7 @@ export async function generateImagePrompt(name: string) {
         },
       ],
     });
-    const imageDescription = response.choices[0]?.message?.content || "";
+    const imageDescription = response.text || "";
     return imageDescription as string;
   } catch (error) {
     console.error(error);
@@ -44,6 +45,32 @@ export async function generateImage(image_description: string) {
       `https://mygrscvbvyqjeebeelev.supabase.co/storage/v1/object/public/potion-storage/` +
       imageUploadData?.path;
     return imageUrl;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function generateCompletion(prompt: string) {
+  try {
+    return streamText({
+      model: groq("llama-3.3-70b-versatile"),
+      messages: [
+        {
+          role: "system",
+          content: `You are a helpful AI embedded in a notion text editor app that is used to autocomplete sentences
+            The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+        AI is a well-behaved and well-mannered individual.
+        AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.`,
+        },
+        {
+          role: "user",
+          content: `I am writing a piece of text in a notion like text editor app.
+        Help me complete my train of thought here: ##${prompt}##
+        keep the tone of the text consistent with the rest of the text.
+        keep the response short and sweet.`,
+        },
+      ],
+    });
   } catch (error) {
     console.error(error);
   }
